2f1bf0fd76a4779b7d898c4b80480bc9ffae68c6
Melanie <git@melanie-busse.de>
2026-04-16 11:01:32 +0200

    insert challenge 3

---
+++ Challenge-3/index.js
@@ -0,0 +1,28 @@
+/*Your task is to sort a given string. Each word in the string will contain a single number.
+This number is the position the word should have in the result.
+
+
+Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+
+If the input string is empty, return an empty string. The words in the input String will only
+contain valid consecutive numbers. */
+
+const data = [
+    "is2 Thi1s T4est 3a",
+    "4of Fo1r pe6ople g3ood th5e the2",
+    ""
+]
+
+function sortString(string) {
+    const words = string.split(' ');
+    const result = [];
+
+    words.forEach(word => {
+        const position = word.match(/\d+/g);
+        result[position - 1] = word;
+    })
+
+    return result.join(' ');
+}
+
+data.map(string =>console.log(sortString(string)));
