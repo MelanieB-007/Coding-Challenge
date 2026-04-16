c9b1e987d098cfb1a3e8c8496d9cd5d85e45dcf3
Melanie <git@melanie-busse.de>
2026-04-16 11:05:22 +0200

    insert tasks

--- Challenge-3/index.js
+++ Challenge-3/index.js
@@ -1,4 +1,5 @@
-/*Your task is to sort a given string. Each word in the string will contain a single number.
+/*
+Your task is to sort a given string. Each word in the string will contain a single number.
 This number is the position the word should have in the result.
 
 
