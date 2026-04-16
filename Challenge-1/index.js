29f1eca0446451766ba304f75cfb26eb61817023
Melanie <git@melanie-busse.de>
2026-04-16 10:17:11 +0200

    insert challenge 1

---
+++ Challenge-1/index.js
@@ -0,0 +1,37 @@
+const data = [
+    {
+        username: 'David',
+        status: 'online',
+        lastActivity: 10
+    },
+    {
+        username: 'Lucy',
+        status: 'offline',
+        lastActivity: 22
+    },
+    {
+        username: 'Bob',
+        status: 'online',
+        lastActivity: 104
+    }
+]
+
+const result = {
+    online: [],
+    offline: [],
+    away: []
+};
+
+data.forEach(user => {
+    if (user.status === 'online') {
+        if (user.lastActivity > 10) {
+            result.away.push(user.username);
+        } else {
+            result.online.push(user.username);
+        }
+    } else if (user.status === 'offline') {
+        result.offline.push(user.username);
+    }
+});
+
+console.log(result);
